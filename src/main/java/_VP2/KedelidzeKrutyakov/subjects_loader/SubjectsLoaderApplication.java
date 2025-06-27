383de25f074fea229795d190281fb71f2826dd1a
Krutyaa <139651197+Krutyaa@users.noreply.github.com>
2025-06-27 13:12:22 +0300

    Initial commit

---
+++ src/main/java/_VP2/KedelidzeKrutyakov/subjects_loader/SubjectsLoaderApplication.java
@@ -0,0 +1,13 @@
+package _VP2.KedelidzeKrutyakov.subjects_loader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubjectsLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubjectsLoaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/_VP2/KedelidzeKrutyakov/subjects_loader/SubjectsLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package _VP2.KedelidzeKrutyakov.subjects_loader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubjectsLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
